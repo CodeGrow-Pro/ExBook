fa4a21f6bb80225e731076d4e3198bba44789313
VishalDevelooperengineer <vk4993102@gmail.com>
2023-03-04 11:10:27 +0530

    page not found issue fixed with hasRouter use

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App /> 
-  </BrowserRouter>
+  </HashRouter>
 );
