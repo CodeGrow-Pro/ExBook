7d01d1b045c75b90b9970983673a9f97431fb99b
VishalDevelooperengineer <vk4993102@gmail.com>
2023-05-29 20:50:40 +0530

    changes

--- client/src/component/Product.jsx
+++ client/src/component/Product.jsx
@@ -1,48 +1,67 @@
-import React, { useEffect, useState } from 'react'
-import BookCart from './BookCart'
-import axios from 'axios'
-import './Product.css'
+import React, { useEffect, useState } from "react";
+import BookCart from "./BookCart";
+import axios from "axios";
+import "./Product.css";
 
 const Product = () => {
-  const [items, setItem] = useState([])
-  useEffect(()=> {
+  let [items, setItem] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  useEffect(() => {
     fetchBooks();
-  }, [])
-  const fetchBooks = ()=>{
+  }, []);
+  const fetchBooks = () => {
     axios({
-      method: 'GET',
-      url: 'https://exbookapis.onrender.com/ExBook/api/v1/book/getBooks'
-    })    
-    .then(res => {
-      setItem(res.data.allBooks)
+      method: "GET",
+      url: "https://exbookapis.onrender.com/ExBook/api/v1/book/getBooks",
     })
-    .catch((err) => {
-      console.log(err);
-    })
-  }
-  
+      .then((res) => {
+        setItem(res.data.allBooks);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearchValue(e.target.value);
+    const values = [];
+    const filteredValue = []
+     items.forEach((ele) => {
+      if (ele.name.toLowerCase().match(searchValue.toLowerCase())) {
+        filteredValue.push(ele)
+      } else {
+        values.push(ele);
+      }
+      setItem([...filteredValue, ...values]);
+    });
+  };
+
   return (
     <div>
-        <section className='shop-card section-p'>
-          <div className="search">
-            <div className="search-input">
-              {/* <form>
-                <input type="text" name="search" placeholder='Book Search here'/>
-                <button className='light-btn'>Search</button>
-              </form> */}
-            </div>
+      <section className="shop-card section-p">
+        <div className="search">
+          <div className="search-input">
+            <form>
+              <input
+                type="text"
+                name="search"
+                onChange={handleSearch}
+                value={searchValue}
+                placeholder="Book Search here"
+              />
+              <button className='light-btn'>Search</button>
+            </form>
           </div>
-          <div className="card-body">
-            {items.map(item => {
-                return (<BookCart 
-                    key={item._id}
-                    Item={item}
-                />)
-              })}
-          </div>
-        </section>
+        </div>
+        <div className="card-body">
+          {items.map((item) => {
+            return <BookCart key={item._id} Item={item} />;
+          })}
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
